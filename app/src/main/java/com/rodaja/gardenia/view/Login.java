8722af38098cdb42888ab230967a205a30b5f3d8
Javi <javierplazasisques@gmail.com>
2020-04-22 21:29:44 +0200

    Interface login acabada

--- app/src/main/java/com/rodaja/gardenia/view/Login.java
+++ app/src/main/java/com/rodaja/gardenia/view/Login.java
@@ -3,15 +3,28 @@ package com.rodaja.gardenia.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.rodaja.gardenia.R;
 
 public class Login extends AppCompatActivity {
+
+    private ImageView ivBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+
+        inicializar();
+
+        Glide.with(this).load(R.drawable.login_background).apply(new RequestOptions().centerCrop()).into(ivBackground);
+    }
+
+    private void inicializar() {
+        ivBackground = findViewById(R.id.ivBackground);
     }
 }
