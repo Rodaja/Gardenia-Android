b6d448fe348e4c75b99e05b4809bb8685a7047a0
Vitter1 <di.viteri.dv@gmail.com>
2020-04-24 15:17:41 +0200

    detalles 0.4

--- app/src/main/java/com/rodaja/gardenia/view/ChooseImage.java
+++ app/src/main/java/com/rodaja/gardenia/view/ChooseImage.java
@@ -2,15 +2,53 @@ package com.rodaja.gardenia.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.rodaja.gardenia.R;
 
 public class ChooseImage extends AppCompatActivity {
+    ImageView ivArrow_back_detaills,iv_plant_one;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_image);
+        getSupportActionBar().hide();
+        inicializar();
+        setImage(R.drawable.arrow_back,ivArrow_back_detaills);
+        setImage(R.drawable.main_plant,iv_plant_one);
+
+        ivArrow_back_detaills.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToDetails(v);
+            }
+        });
+
+
+
     }
+
+
+    private void inicializar() {
+        ivArrow_back_detaills = findViewById(R.id.img_Flecha_volver);
+        iv_plant_one = findViewById(R.id.img_planta_uno);
+
+    }
+
+    private void setImage(Integer resourceId, ImageView imageView) {
+        Glide.with(this).load(resourceId).apply(new RequestOptions().centerCrop()).into(imageView);
+    }
+
+    private void goToDetails(View view){
+        Intent in = new Intent(this, Details.class);
+
+        startActivity(in);
+    }
+
 }
--- app/src/main/java/com/rodaja/gardenia/view/Details.java
+++ app/src/main/java/com/rodaja/gardenia/view/Details.java
@@ -2,15 +2,55 @@ package com.rodaja.gardenia.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.rodaja.gardenia.R;
 
 public class Details extends AppCompatActivity {
+    ImageView ivDetails,ivChoose_plant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        getSupportActionBar().hide();
+
+        inicializar();
+
+        setImage(R.drawable.main_plant,ivDetails);
+        setImage(R.drawable.choose_photo_plot,ivChoose_plant);
+
+        ivChoose_plant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToChooseImage(v);
+            }
+        });
+
     }
+
+    private void inicializar() {
+        ivDetails = findViewById(R.id.img_planta_uno);
+        ivChoose_plant = findViewById(R.id.img_Flecha_volver);
+    }
+
+    /**
+     * Este metodo añade una imagen (R.drawable.*) a una vista
+     * @param resourceId El recurso de la imagen
+     * @param imageView La vista donde se va a incluir la imagen
+     */
+    private void setImage(Integer resourceId, ImageView imageView) {
+        Glide.with(this).load(resourceId).apply(new RequestOptions().centerCrop()).into(imageView);
+    }
+
+    private void goToChooseImage(View view){
+        Intent in = new Intent(this, ChooseImage.class);
+        startActivity(in);
+    }
+
 }
