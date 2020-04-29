71661cf5cf44824a5320aa5df15a63091fda74a2
roberechev <rober.eche.sa@gmail.com>
2020-04-29 13:50:50 +0200

    rama peticiones

--- app/src/main/java/com/rodaja/gardenia/model/web/Request.java
+++ app/src/main/java/com/rodaja/gardenia/model/web/Request.java
@@ -1,5 +1,28 @@
 package com.rodaja.gardenia.model.web;
 
-public class Request {
+import android.content.Context;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class Request {
+    public void getRequest(Context contexto, String url) {
+        RequestQueue queue = Volley.newRequestQueue(contexto);
+        StringRequest stringRequest = new StringRequest(Request., url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+    }
 }
