3d37f319197bf98441616e7999f34fa68ba8a3c4
Javi <javierplazasisques@gmail.com>
2020-04-29 15:50:53 +0200

    Request actualizado

--- app/src/main/java/com/rodaja/gardenia/model/web/Request.java
+++ app/src/main/java/com/rodaja/gardenia/model/web/Request.java
@@ -8,15 +8,18 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+
 public class Request {
+
     public void getRequest(Context contexto, String url) {
         RequestQueue queue = Volley.newRequestQueue(contexto);
-        StringRequest stringRequest = new StringRequest(Request., url,
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
-                        textView.setText("Response is: " + response.substring(0, 500));
+                        textView.setText("Response is: "+ response.substring(0,500));
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -24,5 +27,6 @@ public class Request {
                 textView.setText("That didn't work!");
             }
         });
+
     }
 }
