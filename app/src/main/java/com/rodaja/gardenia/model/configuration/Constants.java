a864715b17f1ad976b763b1027d778780df54c8a
Javi <javierplazasisques@gmail.com>
2020-04-30 14:37:11 +0200

    Peticiones actualizado

--- app/src/main/java/com/rodaja/gardenia/model/configuration/Constants.java
+++ app/src/main/java/com/rodaja/gardenia/model/configuration/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static final int PORT = 8888;
     public static final String IP = "127.0.0.1";
     public static final String URL = "http://" + IP + ":" + PORT;
+    public static final String URL_FLOWERPOT = "api/flowerpots";
+    public static final String URL_USER = "api/users";
 }
