48503ed0216ffd0c382559ab3f53598b6e1d8a24
Vitter1 <di.viteri.dv@gmail.com>
2020-04-30 17:38:56 +0200

    Settings (configuracion) 0.1

---
+++ app/src/main/java/com/rodaja/gardenia/model/configuration/Settings.java
@@ -0,0 +1,15 @@
+package com.rodaja.gardenia.model.configuration;
+
+public class Settings {
+    public static boolean notifications = false;
+    public static String  ut = "°c";
+
+
+    public void default_status(){
+        notifications  = false;
+        ut = "°c";
+    }
+
+}
+
+
