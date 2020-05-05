2242b81cd8eac3805ad58b7aaf595728cad2a921
Javi <javierplazasisques@gmail.com>
2020-05-05 13:53:34 +0200

    Settings actualizado

--- app/src/main/java/com/rodaja/gardenia/model/configuration/Settings.java
+++ app/src/main/java/com/rodaja/gardenia/model/configuration/Settings.java
@@ -2,12 +2,12 @@ package com.rodaja.gardenia.model.configuration;
 
 public class Settings {
     public static boolean notifications = false;
-    public static String  ut = "°c";
+    public static String  temperature = "celsius";
 
 
     public void default_status(){
         notifications  = false;
-        ut = "°c";
+        temperature = "celsius";
     }
 
 }
