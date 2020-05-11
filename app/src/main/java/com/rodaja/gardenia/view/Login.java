60b8af33a8a8f0715cff1df3f4c26d7282480cfe
Javi <javierplazasisques@gmail.com>
2020-05-11 18:18:17 +0200

    Login actualizado

--- app/src/main/java/com/rodaja/gardenia/view/Login.java
+++ app/src/main/java/com/rodaja/gardenia/view/Login.java
@@ -2,12 +2,17 @@ package com.rodaja.gardenia.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -45,6 +50,16 @@ public class Login extends AppCompatActivity {
     private TextInputEditText etEmail;
     private TextInputEditText etPassword;
     private TextView tvSignUp;
+    private CheckBox chboxRecordarme;
+    private User user;
+
+    private String email;
+    private String password;
+
+    private SQLiteOpenHelper sqLiteOpenHelper;
+    private SQLiteDatabase sqLiteDatabase;
+
+    private String nombreBaseDatos = "usuarios";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,14 +71,13 @@ public class Login extends AppCompatActivity {
         contexto = this;
 
         Image.setImage(this, R.drawable.login_background, ivBackground);
-
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 //Obligatorio usar el trim ya que el texto aparece con un espacio al principio
-                String email = String.valueOf(etEmail.getText()).trim();
-                String password = String.valueOf(etPassword.getText()).trim();
+                email = String.valueOf(etEmail.getText()).trim();
+                password = String.valueOf(etPassword.getText()).trim();
 
                 loginRequest(Constants.URL_LOGIN, email, password);
             }
@@ -75,6 +89,11 @@ public class Login extends AppCompatActivity {
                 goToNewView(null, Signup.class);
             }
         });
+
+        setupDataBase();
+        if (userCheckLogin()){
+            loginRequest(Constants.URL_LOGIN, email, password);
+        }
 }
 
     private void inicializar() {
@@ -83,6 +102,8 @@ public class Login extends AppCompatActivity {
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
         tvSignUp = findViewById(R.id.tvSignUp);
+        chboxRecordarme = findViewById(R.id.chboxRecordar);
+
     }
 
     private void loginRequest(String url, String email, String password){
@@ -103,8 +124,13 @@ public class Login extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         Gson gson = new Gson();
                         Log.d("Success", response.toString());
-                        User user = gson.fromJson(response.toString(), User.class);
+                        user = gson.fromJson(response.toString(), User.class);
 
+                        if (chboxRecordarme.isChecked()){
+                            Log.d("Recuerdame", "Recuerdame seleccionado");
+                            guardarDatosUsuario(user);
+                            Log.d("Datos de usario", "Datos usuario guardados");
+                        }
                         Toast toast = Toast.makeText(contexto,
                                 "Bienvenido " + user.getEmail(), Toast.LENGTH_LONG);
                         toast.show();
@@ -145,5 +171,43 @@ public class Login extends AppCompatActivity {
     private void goToNewView(View view, Class goToView) {
         Intent in = new Intent(this, goToView);
         startActivity(in);
+    }
+
+    public void setupDataBase(){
+        sqLiteOpenHelper = new SQLiteOpenHelper(getApplicationContext(),nombreBaseDatos, null,1) {
+            @Override
+            public void onCreate(SQLiteDatabase sqLiteDatabase) {
+                Log.d("OnCreate", "create table");
+                String query = "CREATE TABLE " + nombreBaseDatos + "(email varchar(100), password varchar(100))";
+                sqLiteDatabase.execSQL(query);
+            }
+
+            @Override
+            public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+            }
+        };
+        sqLiteDatabase = sqLiteOpenHelper.getWritableDatabase();
+    }
+    private boolean userCheckLogin() {
+        //Query
+        Cursor cursor =  sqLiteDatabase.rawQuery("SELECT * FROM " +  nombreBaseDatos, null);
+
+        if (cursor.moveToNext()){
+            email = cursor.getString(0);
+            Log.d("Email guardado", email);
+            password = cursor.getString(1);
+            Log.d("Password guardado", password);
+            return true;
+        }
+
+        return false;
+    }
+
+    private void guardarDatosUsuario(User user){
+        ContentValues valores = new ContentValues();
+        valores.put("email", user.getEmail());
+        valores.put("password", password);
+        sqLiteDatabase.insert(nombreBaseDatos, null, valores);
     }
 }
